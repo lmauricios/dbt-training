4cf1e3d84580011de50fb70c2f516e55c936b4f0
leonardofp121@gmail.com <leonardofp121@gmail.com>
2022-09-18 22:23:16 +0000

    stg sem subcategoria

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -16,7 +16,7 @@ c.country,
 p.productid,
 p.category,
 p.productname,
-p.subcategory
+--p.subcategory
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer')}} as c
 on o.customerid = c.customerid
